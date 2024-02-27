a8c15b1cb261924d39e4659bfd646c78d5b45939
Daniel Williams <danielw@jfrog.com>
2024-02-27 12:46:48 -0700

    Adding the example JSON payloads and the scripts to initiate the JFrog Pipelines via webhook call.

---
+++ example-webhook-json/docker-webhook.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ARTI_WEBHOOK_USER="example"
+ARTI_WEBHOOK_PASS="example_password"
+ARTI_WEBHOOK_HOST="lowside.example.com/pipelines/api"
+ARTI_WEBHOOK_NUM="6" # From the JFrog Pipelines Webhook Integration
+
+curl --insecure -T docker.json -H'Content-Type:application/json' -XPOST "https://${ARTI_WEBHOOK_USER}:${ARTI_WEBHOOK_PASS}@${ARTI_WEBHOOK_HOST}/v1/projectIntegrations/${ARTI_WEBHOOK_NUM}/hook"
